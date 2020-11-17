cbc40eef3e8f365d82c08da7efe536c2fc93e178
ht18a110 <ht18a110>
2020-11-17 23:18:42 +0900

    11月17日の作業 main文に入出力例を追加しxた

--- ex-1-main.cpp
+++ ex-1-main.cpp
@@ -14,6 +14,21 @@
 int nth_prime(unsigned int a, unsigned int d, unsigned int n);
 int main() {
 	std::cout << nth_prime(367, 186, 151) << std::endl;
+	std::cout << nth_prime(179, 10, 203) << std::endl;
+	std::cout << nth_prime(271, 37, 39) << std::endl;
+	std::cout << nth_prime(103, 230, 1) << std::endl;
+	std::cout << nth_prime(27, 104, 185) << std::endl;
+	std::cout << nth_prime(253, 50, 85) << std::endl;
+	std::cout << nth_prime(1, 1, 1) << std::endl;
+	std::cout << nth_prime(9075, 337, 210) << std::endl;
+	std::cout << nth_prime(307, 24, 79) << std::endl;
+	std::cout << nth_prime(331, 221, 177) << std::endl;
+	std::cout << nth_prime(259, 170, 40) << std::endl;
+	std::cout << nth_prime(269, 58, 102) << std::endl;
+
+
+
+
 	// 以下、同様に、入出力例通りになるか確認せよ。
 	return 0;
 }
